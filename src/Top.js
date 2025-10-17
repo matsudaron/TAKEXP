09311cbd71dd4ed8514665b4ca26a0c8eedb0e67
rq-matsudaron-at-321899771753 <senntakuita5689@gmail.com>
2025-10-17 02:17:29 +0000

    環境構築

---
+++ src/Top.js
@@ -0,0 +1,19 @@
+// TOP画面
+
+// functionを使った書き方の例
+// function Top() {
+//   return (
+//     <div>
+//     <h1>テキストの例</h1>
+//   </div>
+//   );
+// }
+
+// アロー関数を使った書き方の例
+const Top = () => (
+  <div>
+    <h1>テキストの例</h1>
+  </div>
+);
+
+export default Top;
